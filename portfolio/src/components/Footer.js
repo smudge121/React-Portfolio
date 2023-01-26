3bfaf2b14784c6c0a531fa1d2cf32796b123d51c
Jishii <33364545+smudge121@users.noreply.github.com>
2023-01-25 19:49:30 -0800

    functioning footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
 
 function Footer(){
+    const testStyle = {
+        backgroundColor: "rgba(0, 0, 0, 0.2)" 
+    }
+
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+<div>
+<footer className="bg-light text-center text-lg-start fixed-bottom">
+  <div className="container p-3 text-center">
+    <div className="row d-flex justify-content-around">
+      <a className='col-4' href="https://github.com/smudge121">git</a>
+      <a className='col-4' href="mailto:jaretishii17@gmail.com">Email</a>
+      <a className='col-4' href="https://www.linkedin.com/in/jaret-ishii/">LinkedIn</a>
+    </div>
+  </div>
+
+  <div className="text-center p-3" style={testStyle}>
+    © 2020 Copyright:
+    <a className="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+</footer>
+</div>
     )
 }
 
