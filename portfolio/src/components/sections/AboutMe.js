e5e48447dc1818f5b7f218eafde2acb4a8f48dd3
Jishii <33364545+smudge121@users.noreply.github.com>
2023-01-29 10:56:40 -0800

    started styling about me

--- portfolio/src/components/sections/AboutMe.js
+++ portfolio/src/components/sections/AboutMe.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
+import headshot from "../../assets/JIshii.jpg"
 
 function AboutMe({isActive}){
     return (
         <div>
         {isActive ? (
-          <div>about me</div>
+          <div>about me
+            <div className='container'>
+              <div className='row d-flex'>
+                <img src={headshot} className = "about-img rounded-circle col-3" />
+                <div className='col-9'>
+                  <h2 className='text-left'>Jaret Ishii</h2>
+                </div>
+                
+              </div>
+            </div>
+          </div>
         ) : (
           <div></div>
         )}
