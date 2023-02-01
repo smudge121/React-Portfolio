4c0dec495186cacdf89b063c33dac835756e9d21
Jishii <33364545+smudge121@users.noreply.github.com>
2023-01-31 18:53:47 -0800

    added a form to contact and added 2 more projects

--- portfolio/src/assets/constants/index.js
+++ portfolio/src/assets/constants/index.js
@@ -1,5 +1,7 @@
 import friendImg from "../../assets/friend_finder.png";
 import animeImg from "../../assets/anime_ecommerce.png";
+import wayfarerImg from "../wayfarer.png";
+import noteImg from "../note-taker.png";
 
 export const _projects = [
     {
@@ -15,7 +17,21 @@ export const _projects = [
         git: "https://github.com/thien-le13/anime-ecommerce-webapp",
         url: "https://thien-le13.github.io/anime-ecommerce-webapp/",
         subtitle: "Javascript and APIs"
-    }
+    },
+    {
+        img: wayfarerImg,
+        title: "WayFarer",
+        git: "https://github.com/Lancival/ACMRoadtrip",
+        url: "https://volcanogirl.itch.io/wayfarer",
+        subtitle: "Video game build in Unity"
+    },
+    {
+        img: noteImg,
+        title: "Web Based Note Taker",
+        git: "https://github.com/smudge121/Note-Taker",
+        url: "https://glacial-peak-93009.herokuapp.com/",
+        subtitle: "Express Routes"
+    },
 ]
 
 export default {
--- portfolio/src/components/sections/Contact.js
+++ portfolio/src/components/sections/Contact.js
@@ -7,12 +7,19 @@ function Contact({isActive}){
           <div>
             <div className='container'>
               <div className='row'><h2 className='aboreto display-2 border-bottom border-3 border-dark'>Contact Me</h2></div>
-              <div className='row'>
-                <ul className='mt-5 justify-content-center'>
-                  <li className='mb-2 dark-grey h3 montserrat'><a className='a-over link-custom' href="https://github.com/smudge121"><span className='dancing h1'>Email:</span> jaretishii17@gmail.com</a></li>
-                  <li className='mb-2 dark-grey h3 montserrat'><a className='a-over link-custom' href="mailto:jaretishii17@gmail.com"><span className='dancing h1'>GitHub:</span> smudge121</a></li>
-                  <li className='mb-2 dark-grey h3 montserrat'><a className='dancing h1 a-over link-custom' href="https://www.linkedin.com/in/jaret-ishii/">LinkedIn Page </a></li>
-                </ul>
+              <div className='row mt-3'>
+                <form>
+                  <div className="form-group d-flex flex-column justify-content-center align-items-center mb-3">
+                    <label className="aboreto h2 dark-grey" htmlFor="email-form">Email address</label>
+                    <input type="email" className="form-control-custom w-50 dark-grey montserrat p-2" id="email-form" placeholder="Enter email" />
+                    <small id="emailHelp" className="form-text text-muted">Enter your email to contact me</small>
+                  </div>
+                  <div className="form-group d-flex flex-column justify-content-center align-items-center mb-3">
+                    <label className="aboreto h2 dark-grey" htmlFor="message">Message</label>
+                    <textarea rows="5" className="form-control-custom w-50 dark-grey montserrat p-3" id="message" placeholder="Message of Email" ></textarea>
+                  </div>
+                  <button type="submit" className="rounded-circle p-3 btn-custom h2 dancing">Submit</button>
+                </form>
               </div>
             </div>
           </div>
