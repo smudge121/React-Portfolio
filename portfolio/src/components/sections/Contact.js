5f4331eafa2d0c36ebd3cf8e01dbe7793f062c4c
Jishii <33364545+smudge121@users.noreply.github.com>
2023-01-31 19:04:10 -0800

    added contact me form

--- portfolio/src/components/sections/Contact.js
+++ portfolio/src/components/sections/Contact.js
@@ -1,6 +1,21 @@
 import React, { useState } from 'react';
 
+
 function Contact({isActive}){
+
+  const [message, setMessage] = useState('');
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    window.open(`mailto:jaretishii17@gmail.com?body=${message}`);
+  };
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+
+    setMessage(value);
+  }
+
     return (
         <div>
         {isActive ? (
@@ -16,9 +31,9 @@ function Contact({isActive}){
                   </div>
                   <div className="form-group d-flex flex-column justify-content-center align-items-center mb-3">
                     <label className="aboreto h2 dark-grey" htmlFor="message">Message</label>
-                    <textarea rows="5" className="form-control-custom w-50 dark-grey montserrat p-3" id="message" placeholder="Message of Email" ></textarea>
+                    <textarea rows="5" value={message} onChange={handleInputChange} className="form-control-custom w-50 dark-grey montserrat p-3" id="message" placeholder="Message of Email" ></textarea>
                   </div>
-                  <button type="submit" className="rounded-circle p-3 btn-custom h2 dancing">Submit</button>
+                  <button type="submit" onClick={handleFormSubmit} className="rounded-circle p-3 btn-custom h2 dancing">Submit</button>
                 </form>
               </div>
             </div>
