3dba86cb97ef415dabf7ac0dfd0d75d6fa0798fc
Jishii <33364545+smudge121@users.noreply.github.com>
2023-01-31 19:54:39 -0800

    background fills and scrolls

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,9 +3,9 @@ import Header from './components/Header';
 import Project from './components/Project';
 import Footer from './components/Footer';
 
+
 //import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 //import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
       <Header />
       <Footer />
     </div>
+    
   );
 }
 
--- portfolio/src/components/sections/Resume.js
+++ portfolio/src/components/sections/Resume.js
@@ -7,8 +7,26 @@ function Resume({isActive}){
           <div>
           <div className='container'>
             <div className='row'><h2 className='aboreto display-2 border-bottom border-3 border-dark'>Resume</h2></div>
-            <div className='row'>
-              <p className='mt-5 dancing h1 dark-grey'>To Be Uploaded</p>
+            <div className='row mt-3'>
+              <div className='mb-3 border-bottom border-1 border-dark'>
+                <p className='h1 aboreto dark-grey'>Skills:</p>
+                <div className='d-flex flex-column align-items-center'>
+                  <p className='ft-5 dark-grey montserrat'><span className='h2 dancing dark-grey'>Programming Languages:</span> Javascript, C++, C#, HTML, CSS, SQL</p>
+                  <p className='ft-5 dark-grey montserrat'><span className='h2 dancing dark-grey'>Tools:</span> Unity Game Engine, Visual Studio</p>
+                  <p className='ft-5 dark-grey montserrat'>React, MongoDB, Express, </p>
+                </div>
+              </div>
+              <div className='mb-3 border-bottom border-1 border-dark'>
+                <p className='h1 aboreto dark-grey'>Employment</p>
+                <div className='d-flex flex-column align-items-center'>
+                  <p className='ft-5 dark-grey montserrat'><span className='h2 dancing dark-grey'>Title:</span> Jr Data Analyst</p>
+                  <p className='ft-5 dark-grey montserrat'><span className='h2 dancing dark-grey'>Company:</span> Clever Care Health Plan</p>
+                  <p className='ft-5 dark-grey montserrat'>In this position, I worked with Microsoft SQL server as a data analyst.
+                    I mapped incoming and outgoing data, from and to our data warehouse.
+                  </p>
+                </div>
+              </div>
+              
             </div>
           </div>
         </div>
